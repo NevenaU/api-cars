745870704c857b0ecce37c7940cfc968e689975d
Nevena Uscebrka <nevena.uscebrka94@gmail.com>
2021-02-21 12:08:59 +0100

    auth controller, setup tymon/jwt/auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get("/cars", [CarsController::class, 'index']);
 Route::delete("/cars/{id}", [CarsController::class, 'destroy']);
 Route::get("/cars/{id}", [CarsController::class, 'show']);
-
 Route::post("/cars", [CarsController::class, 'store']);
 Route::put("/cars/{id}", [CarsController::class, 'update']);
+
+Route::post('register', [ AuthController::class, 'register' ]);//->middleware('guest:api');
+Route::post('login', [ AuthController::class, 'login' ]);//->middleware('guest:api');
+Route::post('logout', [ AuthController::class, 'logout' ]);//>middleware('auth:api');
+Route::get('me', [ AuthController::class, 'me' ]);//->middleware('auth:api');
